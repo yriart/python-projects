eab84d77305b6cd48b7f3b37e4a294d7232fa032
Lisa Yriart <eyriart@sailthru.com>
2013-04-29 18:39:22 -0300

    quicksort using recursive list comprehension (not sort-in-place)

---
+++ quicksort.py
@@ -0,0 +1,12 @@
+def quicksort(unsorted_list):
+    if len(unsorted_list)==0:
+        return unsorted_list
+    midpoint = unsorted_list[0]
+    less = quicksort([item for item in unsorted_list[1::] if item <= midpoint])
+    more = quicksort([item for item in unsorted_list[1::] if item > midpoint])
+
+    return less+[midpoint]+more
+
+my_numbers = [7,1,3,4,0,9,6,5,2,8]
+
+print quicksort(my_numbers)
